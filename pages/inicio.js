9531d13da7dab8d83b5116f74bd078dc71238050
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2019-08-28 09:27:23 -0300

    Melhorado o design da página Inicio

--- pages/inicio.js
+++ pages/inicio.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
-import { white } from 'ansi-colors';
+import { View, Text, Button, StyleSheet, ScrollView } from 'react-native';
 
 export default function Inicio() {
     state = {
@@ -19,69 +18,77 @@ export default function Inicio() {
     };
 
     return (
-        <View style={styles.pagina}>
-            <View style={styles.numerosGerais}>
-                <Text style={styles.numerosGerais.numero}>{state.balancoTotal}</Text>
-                <Text style={styles.numerosGerais.titulo}>de balanço total</Text>
-                <Text style={styles.numerosGerais.numero}>{state.investimentoTotal}</Text>
-                <Text style={styles.numerosGerais.titulo}>de investimento total</Text>
+        <ScrollView style={styles.pagina}>
+            <View style={styles.resumo}>
+                <Text style={styles.resumoNumero}>{state.balancoTotal}</Text>
+                <Text style={styles.resumoTexto}>de balanço total</Text>
+                <Text style={styles.resumoNumero}>{state.investimentoTotal}</Text>
+                <Text style={styles.resumoTexto}>de investimento total</Text>
             </View>
             <View style={styles.meses}>
                 {state.meses.map(mes => (
-                    <View style={styles.meses.mes} key={mes.nome}>
-                        <Text style={styles.meses.mes.titulo}>{mes.nome}</Text>
-                        <Text style={styles.meses.mes.entrada}>+ {mes.entrada}</Text>
-                        <Text style={styles.meses.mes.saida}>- {mes.saida}</Text>
-                        <Button title="+ Detalhes" style={styles.meses.mes.detalhes} />
+                    <View style={styles.mes} key={mes.nome}>
+                        <Text style={styles.mesTitulo}>{mes.nome}</Text>
+                        <Text style={styles.mesEntrada}>+ {mes.entrada}</Text>
+                        <Text style={styles.mesSaida}>- {mes.saida}</Text>
+                        <Button title="+ Detalhes" color='magenta' style={styles.mesDetalhes} />
                     </View>
                 ))}
             </View>
-        </View>
+        </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
     pagina: {
-        numerosGerais: {
-            backgroundColor: rgb(52, 152, 219),
-            color: white,
-            numero: {
-                fontSize: '3rem',
-                marginBottom: '0.5rem'
-            },
-            titulo: {
-                fontSize: '1.25rem'
-            }
-        },
-        meses: {
-            backgroundColor: 'magenta',
-            padding: '0.25rem 0',
-            mes: {
-                height: '15rem',
-                width: '17.5rem',
-                margin: '0.5rem',
-                backgroundColor: 'white',
-                borderRadius: '0.2rem',
-                titulo: {
-                    color: 'magenta',
-                    fontWeight: 'bold'
-                },
-                entrada: {
-                    fontSize: '2rem',
-                    fontWeight: 'bold',
-                    marginBottom: '1rem'
-                },
-                saida: {
-                    fontSize: '2rem',
-                    fontWeight: 'bold',
-                    marginBottom: '1rem'
-                },
-                detalhes: {
-                    borderRadius: '2rem',
-                    borderColor: 'magenta',
-                    color: 'magenta'
-                }
-            }
-        }
+    },
+    resumo: {
+        height: 300,
+        backgroundColor: 'rgb(52, 152, 219)',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    resumoNumero: {
+        fontSize: 40,
+        color: '#ffffff'
+    },
+    resumoTexto: {
+        fontSize: 25,
+        color: '#ffffff',
+        marginBottom: 20
+    },
+    meses: {
+        backgroundColor: 'magenta',
+        display: 'flex',
+        padding: 5
+    },
+    mes: {
+        height: 185,
+        margin: 5,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        padding: 10,
+    },
+    mesTitulo: {
+        marginBottom: 10,
+        fontSize: 25,
+        fontWeight: 'bold',
+        color: 'magenta',
+    },
+    mesEntrada: {
+        marginBottom: 10,   
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'green'
+    },
+    mesSaida: {
+        marginBottom: 10,
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'red'
     }
 })
